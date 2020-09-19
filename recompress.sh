32bbdde640cca299cf9c692b547d4afa2362b4af
paw <pasha@home>
2020-09-19 15:01:34 +1200

    success - filtering dirs

--- recompress.sh
+++ recompress.sh
@@ -1,15 +1,17 @@
 export SOURCE="/x/photo/2020"
 export DESTINATION="/d/photo_out"
 # find $SOURCE -maxdepth 1 -mindepth 1 -type d -printf '%f\n'
-for di in $SOURCE/*/     # list directories in the form "/tmp/dirname/"
-do
+raw_dirs=`find $SOURCE -maxdepth 1 -type d`
+all_dirs=`echo "${raw_dirs}" | grep 2020.0[7-8] | grep -v baiduyun`
+echo "${all_dirs}"
+
+while IFS= read -r di; do
     dir=${di%*/}      # remove the trailing "/"
+    echo "==============================="
     cd "$dir"
-    echo =================================
     pwd
-    find . -type f -iname '*.j*' -print0 | 
+    find . -type f -iname '*.j*' -maxdepth 1 -print0 | 
     while IFS= read -r -d '' file; do
         printf '%s\n' "$file"
     done
-
-done
+done <<< "${all_dirs}"
