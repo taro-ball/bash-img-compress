0a6efd1b916f9b74b0b36ab3fc6b8126116bc880
paw <pasha@home>
2020-09-19 19:29:15 +1200

    ad file search regex, still no spaces in files

--- recompress.sh
+++ recompress.sh
@@ -1,8 +1,9 @@
 export SOURCE="/x/photo/2020"
-export DESTINATION="/d/photo_out"
+export DESTINATION="/d/photo_out/"
+export iMGK_PATH="/c/Users/paw/scoop/apps/imagemagick/current"
 # find $SOURCE -maxdepth 1 -mindepth 1 -type d -printf '%f\n'
 raw_dirs=`find $SOURCE -maxdepth 1 -type d`
-all_dirs=`echo "${raw_dirs}" | grep 2020.0[7-8] | grep -v baiduyun`
+all_dirs=`echo "${raw_dirs}" | grep 2020.0[7-8]`
 echo "${all_dirs}"
 
 while IFS= read -r di; do
@@ -10,8 +11,21 @@ while IFS= read -r di; do
     echo "==============================="
     cd "$dir"
     pwd
-    find . -type f -iname '*.j*' -maxdepth 1 -print0 | 
+    out_path=$DESTINATION${dir##*/}
+    mkdir "$out_path"
+    find * -maxdepth 0 -type f -iregex '.*jpg' -print0 |
     while IFS= read -r -d '' file; do
-        printf '%s\n' "$file"
+        #printf '%s\n' "$file"
+
+        # -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace RGB
+        if [ ! -f "$out_path/$file" ];
+        then
+            echo processing $file...
+            convert_cmd="$iMGK_PATH/convert $file -resize 2222x2222 -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace RGB \"$out_path/$file\""
+            eval "$convert_cmd"
+        else
+            echo "$file exists, skipping"
+        fi
+
     done
 done <<< "${all_dirs}"
