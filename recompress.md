f5249aa7edcc1c3258f52314b5488e7ed64442f3
paw <pasha@home>
2020-09-19 13:49:18 +1200

    blah!

--- recompress.md
+++ recompress.md
@@ -1,3 +1,10 @@
+# prereq
+- imagemagic `scoop install imagemagick`
+
 # ideas:
 - pass the desired eg month as a parameter: 2012.05
 - don't do conversion if folder already exists
+
+# imagemagic
+//resize image to width 25, keeping aspect ratio
+convert -geometry 25x src/image1.png out/image1.png
--- recompress.sh
+++ recompress.sh
@@ -1,2 +1,17 @@
-SOURCE="/x/photo/2020"
-find $SOURCE -maxdepth 1 -mindepth 1 -type d -printf '%f\n'
+export SOURCE="/x/photo/2020"
+export DESTINATION="/d/photo_out"
+# find $SOURCE -maxdepth 1 -mindepth 1 -type d -printf '%f\n'
+for di in $SOURCE/*/     # list directories in the form "/tmp/dirname/"
+do
+    dir=${di%*/}      # remove the trailing "/"
+    echo $dir
+#    echo ${dir##*/}    # print everything after the final "/"
+#    LIST=$(find \"$dir\" -name *.jpg)
+    find \"$dir\" -name "*.jpg" -type f -print0 |
+    while read -d '' file
+    do
+        echo "<<$file>>"
+    done
+    #echo $LIST
+done
+
